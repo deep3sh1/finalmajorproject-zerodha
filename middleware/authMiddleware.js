a12123a4f0f71ff3eae15d426ca76e07ad2b6f3b
deepesh wadhe <deepeshw3108@gmail.com>
2025-12-10 15:10:12 +0530

    Initial commit for backend

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const authHeader = req.header("Authorization");
+
+    if (!authHeader) {
+      return res.status(401).json({ message: "No token, access denied" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const verify = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = verify;
+    next();
+  } catch (err) {
+    console.error("JWT ERROR:", err.message);
+    return res.status(401).json({ message: "Token is not valid" });
+  }
+};
